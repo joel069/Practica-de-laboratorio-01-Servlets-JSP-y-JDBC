6d85c9c402581b316183abd19eb4d6c5c10ba309
Joel <Joel Vasquez@JoelVasquez>
2020-05-03 19:56:45 -0500

    Correccion Base Datos

--- Agenda_Telefonica/src/utils/Conexion.java
+++ Agenda_Telefonica/src/utils/Conexion.java
@@ -13,7 +13,7 @@ package utils;
 		public static void main(String[] args) throws SQLException {
 			Connection conexion = null;
 			Statement sentencia = null;
-			ResultSet result = null;
+			
 			
 			String url ="jdbc:mysql://localhost:3306/Agenda?serverTimezone=UTC";
 			String user = "root";
@@ -24,28 +24,9 @@ package utils;
 				conexion = DriverManager.getConnection(url, user, pass);
 				sentencia = conexion.createStatement();
 			}catch(ClassNotFoundException e) {
-				System.out.println("Imposible Conectar:" + e.getMessage());
+				System.out.println("Error de Conexion....@!!!:" + e.getMessage());
 			}
-			try {
-				sentencia.executeUpdate("DROP TABLE IF EXISTS tabla1");
-				sentencia.executeUpdate("CREATE TABLE tabla1 (id1 INT PRIMARY KEY ,nombre CHAR(20) DEFAULT 's/n')");
-			}catch(SQLException e) {
-				System.out.println("Creacion de tabla fallida...!!!" + e.getMessage());
-			}
-			try {
-				sentencia.executeUpdate("INSERT tabla1 (id1) VALUES (3)");
-				sentencia.executeUpdate("INSERT tabla1  VALUES (4, 'Jesus')");
-				sentencia.executeUpdate("INSERT tabla1  VALUES (5, 'Juan')");	
-			}catch(SQLException e) {
-				System.out.println("Insercion de datos fallida....!!!!!	" + e.getMessage());
-			}
-			try {
-				result = sentencia.executeQuery("SELECT * FROM tabla1");
-				while (result.next())
-					System.out.println("id1: " + result.getLong("id1") + ", nombre:" +result.getString("nombre"));	
-			} catch(SQLException e) {
-				System.out.println("Consulta no se a podido realizar...!!" + e.getMessage());
-		 }
+			
 	   } 
 	}
 
